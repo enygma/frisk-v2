0f597926090f64fc582fb5e6fde861314beefe5e
Chris Cornutt <enygma@phpdeveloper.org>
2011-04-18 20:35:19 -0500

    documentation update for assertions

--- Frisk/Assert.php
+++ Frisk/Assert.php
@@ -1,18 +1,53 @@
 <?php
 namespace Frisk;
 
+/**
+ * Assertion base class
+ *
+ * @package frisk-v2
+ * @author Chris Cornutt <ccornutt@phpdeveloper.org>
+ */
 class Assert {
 
+	/**
+	 * Assertion type
+	 * @var string
+	 */
 	private $_type		= null;
-	private $_passFail	= false;
+	
+	/**
+	 * Pass/fail status
+	 * @var bool
+	 */
+	private $_status	= false;
 
+	/**
+	 * Assertion method
+	 *
+	 * @return null
+	 */
 	public function assert(){}
-		
-	public function setPass($status)
+	
+	/**
+	 * Set Pass/Fail status
+	 * 
+	 * @param bool $status Pass/Fail status
+	 * @return null
+	 */	
+	public function setStatus($status)
 	{
+		if(!is_bool($status)){
+			throw new Exception('Status must be boolean!');
+		}
 		$this->_passFail = $status;
 	}
-	public function getPass()
+	
+	/**
+	 * Get the current pass/fail status
+	 * 
+	 * @return bool $_passFail Pass/fail status
+	 */
+	public function getStatus()
 	{
 		return $this->_passFail;
 	}
--- Frisk/Assert/Contains.php
+++ Frisk/Assert/Contains.php
@@ -2,11 +2,32 @@
 namespace Frisk\Assert;
 use Frisk\Assert as Assert;
 
+/**
+ * Assertion to check the response for a certain string
+ *
+ * @package frisk-v2
+ * @subpackage assertions
+ * @author Chris Cornutt <ccornutt@phpdeveloper.org>
+ */
 class Contains extends Assert
 {
+	/**
+	 * Type of the assertion
+	 * @var string
+	 */
 	private $_type 			= 'contains';
+	
+	/**
+	 * Case-sensitive setting
+	 * @var bool
+	 */
 	private $_caseSensitive	= true;
 	
+	/**
+	 * Set the case-sensitive setting for this assertion
+	 *
+	 * @param bool $bool Case-sensitive toggle
+	 */
 	public function setCaseSensitive($bool)
 	{
 		if($bool !== true && $bool !== false){
@@ -15,6 +36,12 @@ class Contains extends Assert
 		$this->$_caseSensitive = $bool;
 		return $this;
 	}
+	
+	/**
+	 * Return the case-sensitive setting for the current assertion
+	 *
+	 * @return bool $_caseSensitive Case-sensitive toggle
+	 */
 	public function getCaseSensitive()
 	{
 		return $this->_caseSensitive;
@@ -22,13 +49,17 @@ class Contains extends Assert
 	
 	/**
 	 * Check the given request's response to see if it contains the string
+	 *
+	 * @param object $request HttpMessage object
+	 * @param string $containsString String to check body content for
+	 * @return null
 	 */
 	public function assert($request,$containsString)
 	{
 		$content 	= $request->_responseObject->getBody();
 		$status 	= ($this->_caseSensitive===true) ? (bool)strpos($content,$containsString) : (bool)stripos($content,$containsString);
 		
-		($status==true) ? $this->setPass(true) : $this->setPass(false);
+		($status==true) ? $this->setStatus(true) : $this->setStatus(false);
 	}
 	
 }
