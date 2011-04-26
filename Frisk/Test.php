e735c595034ce87d0e8dbe36d6a223320481aa25
Chris Cornutt <enygma@phpdeveloper.org>
2011-04-26 21:29:37 +0000

    updating to change Test over to the "request collection" idea

--- Frisk/Test.php
+++ Frisk/Test.php
@@ -7,9 +7,70 @@ namespace Frisk;
  * @package frisk-v2
  * @author Chris Cornutt <ccornutt@phpdeveloper.org>
  */
-class Test
+class Test implements \ArrayAccess,\IteratorAggregate
 {
-	// nothing here yet
+	private $_requestSet = array();
+
+	public function __construct()
+	{
+		//nothing yet
+	}
+
+	// ----------------------
+	// Iterator methods
+	
+	public function offsetGet($offset)
+        {
+                return (isset($this->_requestSet[$offset])) ? $this->_requestSet[$offset] : null;
+        }
+        public function offsetSet($offset,$value)
+        {
+                $this->_requestSet[] = $value;
+        }
+        public function offsetUnset($offset)
+        {
+                if(isset($this->_requestSet[$offset])){ unset($this->_requestSet[$offset]); }
+        }
+        public function offsetExists($offset)
+        {
+                return (isset($this->_requestSet[$offset])) ? true : false;
+        }
+        public function getIterator()
+        {
+                return new \ArrayIterator($this->_requestSet);
+        }
+
+        // --------------------------------
+        // Custom methods
+
+	public function loadRequestsFromFile($testFilePath)
+	{
+		if(is_file($testFilePath)){
+			// load in our file, grab the class name and reflect on it
+			$testFileContents = file_get_contents($testFilePath);
+			if(preg_match('/class (.*?) extends Test/',strtolower($testFileContents),$classDetailMatch)){
+				require_once($testFilePath);
+				// our class name should be in $classDetailMatch[1]
+
+				$reflectClass = new ReflectionClass($classDetailMatch[1]);
+				$methods = $reflectClass->getMethods();
+				$testInstace = new $classDetailMatch();
+
+				// loop through the methods, find the test*
+				foreach($methods as $method){
+					if(substr($method->name,0,4) == 'test'){
+						echo $method->name."\n";	
+					}
+				}
+				
+			}else{
+				throw new Exception('Class file invalid: '.$testFilePath);
+			}
+		}else{
+			throw new Exception('Cannot read test file: '.$testFilePath);
+		}
+	}
+	
 }
 
 ?>
